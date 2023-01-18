6d5a5276b88609875d39b168da5262b301927549
gabriel <puppizin@gmail.com>
2023-01-18 14:45:27 -0300

    Initial Commit

---
+++ CuWeather/CuWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  CuWeatherApp.swift
+//  CuWeather
+//
+//  Created by Gabriel Puppi on 18/01/23.
+//
+
+import SwiftUI
+
+@main
+struct CuWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
